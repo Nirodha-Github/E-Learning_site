bd23ff3ce33ae29fdcc0aa66ba795233d1385850
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2020-12-28 15:49:59 +0530

    Update games.php

--- games.php
+++ games.php
@@ -180,9 +180,9 @@ if(isset($_SESSION['user_email'])){
         </div>
     </footer>
     <!-- jQuery first, then Popper.js, then Bootstrap JS. -->
-    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
-    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
-    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
