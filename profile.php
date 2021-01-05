5e0072d2f51ee905853425d146a15be178bc9d8e
Nirodha Lakmali <58288134+Nirodha-Github@users.noreply.github.com>
2021-01-05 19:59:25 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,32 +2,55 @@
 require_once "pdo.php";
 session_start();
 
-
 $user_email=$_SESSION['user_email'];
 
+$sql="SELECT * from signup where email=:eml";
+$stmt=$pdo ->prepare($sql);
+$stmt -> bindParam(':eml',$user_email,PDO::PARAM_STR);
+$stmt->execute();
+$result=$stmt -> fetchAll(PDO::FETCH_ASSOC);
 
+if ($stmt->rowCount()>0) {
+   
+  foreach ($result as $row) {
+    $U_fname=$row['firstname'];
+    $U_lname=$row['lastname'];
+    $U_phone=$row['phonenumber'];
+    $U_email=$row['email'];
+    $U_bio = $row['biography'];
+    $U_pic = $row['profile_pic'];
+    
+
+  }
+}
 
 if(isset($_POST['update'])){
-
-     
+   
         $firstname=$_POST['firstname'];
         $lastname=$_POST['lastname'];
         $phonenumber=$_POST['phonenumber'];
         $email=$_POST['email'];
+        $bio=$_POST['bio'];
+        $profile=$_POST['picture'];
+       
 
-        $sql = "UPDATE signup set firstname =  :firstname, lastname = :lastname, phonenumber = :phonenumber, email = :email where email = $user_email";
+    $sql = "UPDATE signup set firstname =  :firstname, lastname = :lastname, phonenumber = :phonenumber, email = :email, biography = :bio, profile_pic = :pic where email =:email";
         $stmt = $pdo->prepare($sql);
         $stmt -> bindParam(':firstname',$firstname,PDO::PARAM_STR);
         $stmt -> bindParam(':lastname',$lastname,PDO::PARAM_STR);
         $stmt -> bindParam(':phonenumber',$phonenumber,PDO::PARAM_STR);
         $stmt -> bindParam(':email',$email,PDO::PARAM_STR);
+        $stmt -> bindParam(':bio',$bio,PDO::PARAM_STR);
+        $stmt -> bindParam(':pic',$profile,PDO::PARAM_STR);
 
         $stmt->execute();
         
         $_SESSION['user_email'] = $email;
         $user_email=$_SESSION['user_email'];
-          
+        $row=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
 }
+       
 
 ?>
 
@@ -41,12 +64,22 @@ if(isset($_POST['update'])){
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="node_modules/bootstrap-social/bootstrap-social.css">
         <link rel="stylesheet" type="text/css" href="css\style.css">
+        <style type="text/css"> 
+        
+         input[type="text"],input[type="textarea"],input[type="email"],input[type="tel"]{
+          font-family:Lucida Console ;
+
+         }
+
+        </style>
 	      <title>Happy Learn-Home</title>
 
     </head>
-
+    
     <body>
     
     	<nav class="navbar navbar-dark navbar-expand-sm fixed-top">
@@ -85,8 +118,8 @@ if(isset($_POST['update'])){
           echo '<div class="dropdown">
                   <button type="button" id="user" class="btn btn-rounded btn-outline-light dropdown-toggle" data-toggle="dropdown"><b>'.$user_email.'</b></button>
                   <div class="dropdown-menu">
-                      <a class="dropdown-item" href="#">Profile</a>
-                      <a class="dropdown-item" href="logout.php">Logout</a>
+                      <a class="dropdown-item" href="#"><i class="fa fa-id-card" aria-hidden="true"></i> Profile</a>
+                      <a class="dropdown-item" href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a>
                   </div>
             </div>';
         }
@@ -109,72 +142,61 @@ if(isset($_POST['update'])){
 
       <div class="container">
         <div class="row">
-        <form method="post"  class="card col-12 col-md-12 m-5 p-5">
-            <div class="form-group row card-header bg-secondary text-white">
+
+        <form method="post"  class="card col-12 col-md-12 m-5 p-5" enctype="multipart/form">
+           <div class="form-group row card-header bg-dark text-white">
                     <label class="col-12 col-md-6 offset-md-3 text-center"><h1><i class="fa fa-user-circle fa-lg"></i>&nbsp&nbspProfile</h1></label>
             </div>
             <div class="card-body row"> 
-      <?php
-                  
-           if ( isset($_SESSION["error"]) ) {
-               echo('<p class="alert alert-danger alert-dismissible fade show" role="alert"  style="color:red"><b>'.$_SESSION["error"]."</b></p>\n");
-               unset($_SESSION["error"]);
-               
-              }
-           if(isset($_SESSION["success"])){
-               echo('<p class="alert alert-success alert-dismissible fade show" role="alert"  style="color:green"><b>'.$_SESSION["success"]."</b></p>\n");
-               unset($_SESSION["success"]);
-              }
-
-               ?>
              <div class="col-12 col-md-3 order-md-first mr-5 offset-md">
 
-              <div class="form-group row pic border border-secondary">
+              <div class="form-group row pic border border-dark">
                    <input type="file" id="file" name="picture">
-                   <img src="image/profile_img" id="profile_pic" width="250"  >
+                   <img src="image/<?php echo $U_pic;?>"  id="profile_pic" width="250"  ></input> 
               </div>
 
+
            </div>
-            <div class="col-12 col-md-8 order-md-last border-secondary border-left">        
+            <div class="col-12 col-md-8 order-md-last border-dark border-left">        
                   <div class="form-group row">
                         <label for="bio" class="col-12 col-md-5 col-form-label">Biography</label>
                         <div class="col-md-7">
-                            <input type="textarea" rows="5" name="bio" class="form-control" id="bio" placeholder="">
+                            <input type="textarea" value="<?php echo $U_bio;?>" rows="5" name="bio" class="form-control" id="bio" placeholder="">
                         </div>
                     </div>  
                    <div class="form-group row">
                         <label for="firstname" class="col-12 col-md-5 col-form-label"><span></span>First Name</label>
                         <div class="col-md-7">
-                            <input type="text" name="firstname" class="form-control" value="" id="firstname"  placeholder="">
+                            <input type="text" name="firstname" class="form-control" value="<?php echo $U_fname; ?>" id="firstname"  placeholder="First Name">
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="lastname" class="col-12 col-md-5 col-form-label">Last Name</label>
                         <div class="col-md-7">
-                            <input type="text" name="lastname" class="form-control" value="" id="lastname"  placeholder=""></input>
+                            <input type="text" name="lastname" class="form-control" value="<?php echo $U_lname; ?>" id="lastname"  placeholder=""></input>
                         </div>
                     </div> 
                     <div class="form-group row">
                         <label for="phonenumber" class="col-12 col-md-5 col-form-label">Phone Number</label>
                         <div class="col-md-7">
-                            <input type="tel" name="phonenumber" class="form-control" id="phonenumber" value="" size="10" maxlength="10" placeholder=""></input>
+                            <input type="tel" name="phonenumber" class="form-control" id="phonenumber" value="<?php echo $U_phone; ?>" size="10" maxlength="10" placeholder="Phone Number"></input>
                         </div>
                     </div> 
                     <div class="form-group row">
                         <label for="email" class="col-12 col-md-5 col-form-label">Email</label>
                         <div class="col-md-7">
-                            <input type="email" name="email" class="form-control text-dark" id="email" value="<?php echo $user_email;?>"></input>
+                            <input type="email" name="email" class="form-control text-dark" id="email" value="<?php echo $U_email; ?>"></input>
                         </div>
                     </div>     
                     <div class="form-group row">
                         <div class="col-md-4 offset-md-1">
-                            <input name="update" type="submit" value="Edit Profile" id="update" class="btn btn-outline-secondary update">
+                            <input name="update" type="submit" value="Save Update" id="update" class="btn btn-dark update">
                         </div>  
                     </div>
               </div>
               </div>
-           
            </form>
+      
          </div> 
       </div> 
        
@@ -222,9 +244,9 @@ if(isset($_POST['update'])){
         </div>
     </footer>
     <!-- jQuery first, then Popper.js, then Bootstrap JS. -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script type="text/javascript">
       
       window.addEventListener('load',function(){
